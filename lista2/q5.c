bb4f57f22d07468fc5632ab2afbb20c423820073
soyEldvd <jdca.dvd@alu.ufc.br>
2022-08-26 17:05:11 -0300

    maior e maior do vetor

---
+++ lista2/q5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void diff();
+int main (){
+    int n, i;
+
+    printf("digite o tamanho do vetor: ");
+    scanf("                      %d", &n);
+
+    int l[n];
+
+    for(i = 0; i < n; i++){
+        printf("digite o %d valor do vetor: ", i + 1);
+        scanf("%d", &l[i]);
+    }
+
+    diff(l, n);
+
+    return 0;
+}
+
+void diff(int l[], int n){
+    int maior, menor, j;
+
+    for (j = 0; j < n; j ++){
+        if(j == 0 ){
+            maior = l[j];
+            menor = l[j];
+        }
+        else {
+            if(l[j] > maior){
+                menor = maior;
+                maior = l[j];
+            }
+        }
+        
+    }
+
+    printf("maior: %d \nmenor: %d", maior, menor);
+
+}
