30c6db036a5ead68d39ea7c4f88a26c1fbc843b4
soyEldvd <jdca.dvd@alu.ufc.br>
2022-09-08 03:53:02 -0300

    lista 2

--- lista2/q9.c
+++ lista2/q9.c
@@ -19,12 +19,12 @@ int main (){
 }
 
 void repet(int l[], int n){
-    int j, k, repeat;
+    int j, c, repeat;
 
     for (j = 0; j < n; j++){
-        for(k = j + 1; k < n; k++){
-            if(l[j] == l[k]){
-                repeat = l[k];
+        for(c = j + 1; c < n; c++){
+            if(l[j] == l[c]){
+                repeat = l[c];
             }
         }
     }
