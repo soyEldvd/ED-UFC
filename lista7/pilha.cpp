0c3b8d6ae58376da37aa2977a2e7fc3dbe04abff
soyEldvd <jdca.dvd@alu.ufc.br>
2022-10-14 21:23:07 -0300

    pilha - adição de comentários

--- lista7/pilha.cpp
+++ lista7/pilha.cpp
@@ -12,28 +12,28 @@ class pilha{
         pilha (int tamanho){
             v = new int[tamanho]; //o mesmo que v[tamanho]
             tamax = tamanho; //o tamanho máximo será o desejado
-            topo = -1; 
-            count = 0;
+            topo = -1; //o topo inicia no -1 pois assim nos certificaremos que não há nenhum elemento em sua frente, note que se ele começasse no 0, inseriríamos o primeiro item da pilha na segunda posição
+            count = 0;//numero de elementos durante a iteração
         }
         
         //metodo empilhar
-        void empilhar (int x){
-            if(topo + 1 < tamax){
-                v[topo + 1] = x;
-                count++;
-                topo++;
+        void empilhar (int x){ 
+            if(topo + 1 < tamax){ //se a posiçaõ topo for menor que o máximo o tamanho máximo da pilha, execute:
+                v[topo + 1] = x; //o valor alocado na posição topo+1 do vetor será o de x
+                count++; //existem agora count++ elementos na pilha
+                topo++; //vamos colocar mais um elemento na pilha
             }
-            else{
+            else{ // se a posição topo for maior, a pilha tá cheia
                 printf("vetor ta cheio\n");
             }
         }
         
         //metodo desempilhar
         int desempilhar(){
-            int aux = v[topo];
+            int aux = v[topo]; //um aux receberá o valor da posição topo
             
-            if(count != 0){
-                topo--;
+            if(count != 0){ //se existir qualquer elemento na pilha: 
+                topo--; //eu tiro ele
                 count --;
                 return aux;
             }
@@ -50,11 +50,11 @@ class pilha{
 int main(){
     int tamanho = 10;
     int i;
-    pilha a(tamanho);
+    pilha a(tamanho); //chamando o método na função principal
     
     
     for(i = 0; i < tamanho; i++){
-        a.empilhar(i);
+        a.empilhar(i);//inserindo valores na pilha
     }
     
     return 0;
